11d709e25869d83a5be11390aad6143b009b9747
Kevin Fechner <developer.kevinfechner@gmail.com>
2019-03-12 18:40:26 +0100

    git init

---
+++ bbb-bac-importer.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: BigBoxBerlin Überweisungsimporter
+Plugin URI: https://
+Description: BigBoxBerlin Überweisungsimporter
+Version: 0.1.0
+Author: Kevin Fechner
+Author URI: https://complete-webolutions.de
+Text Domain: bbb-bac-importer
+Domain Path: /languages
+ */
+
+if (! defined('ABSPATH')) exit;
+
+define('BBB_BAC_IMPORTER_PATH', WP_PLUGIN_DIR . '/bbb-bac-importer/');
